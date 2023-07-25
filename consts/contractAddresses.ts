e9bf04081b892578b0d4e5c95d35ef85ab2d6c03
voldeck-fledge <m.voldeck@gmail.com>
2023-07-25 16:02:15 -0400

    second commit

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0xFd84F37e1c85b1Cd61e54021a6a1561F467eb1c2";
+export const tokenContractAddress =
+  "0xF1bC1eF1Ace1b6BE0aB70b2568380Ce78BB870c2";
+export const stakingContractAddress =
+  "0xBdB8440c256f3BfE47f804ac4b4BD881764119f0";
