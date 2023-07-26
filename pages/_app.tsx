e23d40d9fc343b3c4ce5986bf633296ef59d9e89
voldeck-fledge <m.voldeck@gmail.com>
2023-07-25 21:25:30 -0400

    added clientid

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,11 @@ import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
 const activeChain = "polygon";
+const clientId = "1fcc01a11ad913c9eb655bf60733f445";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={activeChain} clientId="1fcc01a11ad913c9eb655bf60733f445">
+    <ThirdwebProvider activeChain={activeChain} clientId={clientId}>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
