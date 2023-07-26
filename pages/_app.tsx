da0e0a0360c19db6ea195227cef17955caa8d74b
voldeck-fledge <m.voldeck@gmail.com>
2023-07-25 21:19:21 -0400

    added clientid

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ const activeChain = "polygon";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider activeChain={activeChain} clientId="1fcc01a11ad913c9eb655bf60733f445">
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
